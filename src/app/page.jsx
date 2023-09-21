c362cd762a87db7694d5e0cadb4b01bca3c9dc81
k.stopczynska <k.stopczynska@gmail.com>
2023-09-21 15:00:23 +0200

    basic tailwind config

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <div className="text-red-500 text-6xl">Hello</div>;
+  return <div >Hello</div>;
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,28 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
+  darkMode: ["class"],
   content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
-    extend: {},
-  },
-  plugins: [],
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
+    extend: {
+      fontFamily: {
+        sans: ["Open sans", ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        'primary-100': '#1A1423', //almost black
+        'primary-200': '#8D818C', //gray taupe  
+        'secondary-100': '#119DA4', //dark cyan 
+        'danger-150': '#307473', //myrtle green
+        'light-100': '#E4FDE1', //light greenish
+      },
+    },
+    plugins: [],
+  }
 }
-
