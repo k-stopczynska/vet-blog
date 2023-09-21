4da49dafebddfcc063fe60a649d5514495acf30e
k.stopczynska <k.stopczynska@gmail.com>
2023-09-21 16:03:49 +0200

    important change in metadata for client

--- src/app/layout.js
+++ src/app/layout.js
@@ -7,7 +7,7 @@ const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Wyprany z empatii',
-  description: 'Weterynaryjny głos z piwnicy',
+  description: 'Weterynaryjny głos z płyty gnojowej',
 }
 
 export default function RootLayout({ children }) {
