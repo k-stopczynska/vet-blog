dcac6fde949cc8d9eb6d34313a7f9f65ef028a63
k.stopczynska <k.stopczynska@gmail.com>
2023-09-26 19:23:59 +0200

    added api endpoint for fetching and creating comments

---
+++ src/app/api/comments/route.js
@@ -0,0 +1,44 @@
+import { getAuthSession } from "@/utils/auth";
+import prisma from '@/utils/connect';
+import { NextResponse } from 'next/server';
+
+
+export const GET = async (req) => {
+    const { searchParams } = new URLSearchParams(req.url);
+
+    const postSlug = searchParams.get('postSlug');
+
+    try {
+        const comments = await prisma.comment.findMany({
+            where: {
+                ...postSlug && { postSlug }
+            },
+            include: { user: true },
+        });
+
+        return new NextResponse(JSON.stringify(comments, {status: 200}))
+    } catch (err) {
+        return new NextResponse({message: 'Error fetching comments to this post'}, {status: 500})
+    }
+}
+
+export const POST = async () => {
+    const session = getAuthSession();
+    if (!session) {
+        return new NextResponse(
+            JSON.stringify({ message: 'You are not authorized!' }, { status: 401 }))
+    }
+
+    try {
+        const body = await req.json();
+        const comment = await prisma.comment.create({
+            data: {...body, userEmail: session.user.email}
+        })
+
+        return new NextResponse(JSON.stringify(comment, {status: 200}))
+    } catch (err) {
+        return new NextResponse(
+            JSON.stringify({ message: 'Something went wrong' }, { status: 500 })
+        )
+    }
+}
