41c3f4026454f11c09fb11676a2268e7117fedbc
k.stopczynska <k.stopczynska@gmail.com>
2023-09-27 20:42:26 +0200

    fixed z-index on mobile nav

--- src/components/AuthLink.jsx
+++ src/components/AuthLink.jsx
@@ -1,6 +1,6 @@
 'use client';
 import React, { useState } from 'react';
-import {signIn,signOut, useSession} from 'next-auth/react'
+import { signIn, signOut, useSession } from 'next-auth/react';
 import Link from 'next/link';
 import Button from './Button';
 
@@ -15,8 +15,8 @@ const AuthLink = () => {
 	const className = !isOpen
 		? ' rotate-[90deg] before:translate-x-[-11px]  after:rounded after:translate-x-[7px]'
 		: ' rotate-[45deg]  before:rotate-[-90deg] before:translate-x-[-3px]  before:translate-x-[-13px] after:translate-x-[-2px]';
-	
-	const navClass = !isOpen ? 'translate-x-[800px]' : 'translate-x-[0px]'
+
+	const navClass = !isOpen ? 'translate-x-[800px]' : 'translate-x-[0px]';
 
 	return (
 		<>
@@ -57,7 +57,7 @@ const AuthLink = () => {
 				</button>
 				{isOpen && (
 					<div
-						className={`w-full h-full flex flex-col gap-12 items-center justify-center bg-primary-100 absolute top-24 left-0 text-4xl transition-all ${navClass}`}
+						className={`w-full h-full flex flex-col gap-12 items-center justify-center bg-primary-100 absolute top-24 left-0 text-4xl transition-all z-50 ${navClass}`}
 					>
 						<Link href='/blog' className='link '>
 							Blog
