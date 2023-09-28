ddea1e4bf3ecf1b448189332ac833edd2ce60555
k.stopczynska <k.stopczynska@gmail.com>
2023-09-28 16:31:21 +0200

    added plugin for monorepo prisma

--- next.config.js
+++ next.config.js
@@ -1,7 +1,20 @@
 /** @type {import('next').NextConfig} */
+const { PrismaPlugin } = require('@prisma/nextjs-monorepo-workaround-plugin')
+
+
 const nextConfig = {
      images: {
     domains: ["lh3.googleusercontent.com"],
+  },
+}
+
+module.exports = {
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.plugins = [...config.plugins, new PrismaPlugin()]
+    }
+
+    return config
   },
 }
 
