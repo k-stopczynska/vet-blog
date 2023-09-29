284ba0bb0a60f19ba61355ee1a70866ec2f60e1a
k.stopczynska <k.stopczynska@gmail.com>
2023-09-29 13:23:47 +0200

    added import

--- src/app/write/page.jsx
+++ src/app/write/page.jsx
@@ -1,6 +1,7 @@
 'use client';
 import React from 'react';
 import { useRouter } from 'next/navigation';
+import Link from 'next/navigation';
 import { useSession } from 'next-auth/react';
 import Button from '@/components/Button';
 
