ced1be5427de677b08cc509904284babc076ad7c
k.stopczynska <k.stopczynska@gmail.com>
2023-09-29 12:20:19 +0200

    debug 15

--- src/app/api/posts/[slug]/route.js
+++ src/app/api/posts/[slug]/route.js
@@ -7,7 +7,7 @@ export const GET = async (req, { params }) => {
     const post = await prisma.post.update({
       where: { slug },
       data: { views: { increment: 1 } },
-      // include: { user: true },
+      include: { user: true },
     });
     return new NextResponse(JSON.stringify(post, { status: 200 }));
   } catch (err) {
--- src/app/layout.js
+++ src/app/layout.js
@@ -11,7 +11,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en-GB">
       <AuthProvider>
         <body className="bg-primary-100 font-plex text-light-100 w-full">
           <Wrapper>
