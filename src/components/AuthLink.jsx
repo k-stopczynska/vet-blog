2711337378740a868f7701ebad99efb0e33b523a
k.stopczynska <k.stopczynska@gmail.com>
2023-09-29 14:15:36 +0200

    protected routes for user

--- src/components/AuthLink.jsx
+++ src/components/AuthLink.jsx
@@ -32,7 +32,7 @@ const AuthLink = () => {
 			)}
 			{status === 'authenticated' && (
 				<>
-					{data.user.name === 'Klaudia Stopczyńska' && (
+					{data.user.name === 'Paweł Stopczyński' && (
 						<Link href='/write' className='link hidden md:flex'>
 							Napisz
 						</Link>
@@ -53,7 +53,7 @@ const AuthLink = () => {
 				<button
 					className='h-[40px] w-[40px] '
 					onClick={toggleMenu}
-					aria-label="hamburger menu"
+					aria-label='hamburger menu'
 				>
 					<div
 						className={`${className} absolute h-[40px] w-[4px] bg-secondary-100 rounded translate-x-[21px] translate-y-[-50%] before:absolute before:h-[40px] before:w-[4px] before:bg-light-100 before:rounded after:absolute after:h-[40px] after:w-[4px] after:bg-light-100 transition-all`}
@@ -85,7 +85,7 @@ const AuthLink = () => {
 						)}
 						{status === 'authenticated' && (
 							<>
-								{data.user.name === 'Klaudia Stopczyńska' && (
+								{data.user.name === 'Paweł Stopczyński' && (
 									<Link
 										href='/write'
 										className='link flex md:hidden'
