b503b9603ac7791a62a9283e8eb51901e13c585c
k.stopczynska <k.stopczynska@gmail.com>
2023-09-29 13:03:56 +0200

    debug 18 after netlify plugin

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,16 +1,15 @@
 import Featured from '@/components/Featured';
 import CardList from '@/components/CardList';
 import CategoryList from '@/components/CategoryList';
-import RootLayout from './layout';
 
 export default function Home({ searchParams }) {
 	const page = parseInt(searchParams.page) || 1;
 
 	return (
-		<RootLayout>
+		<>
 			<Featured />
 			<CategoryList />
 			<CardList page={page} />
-		</RootLayout>
+		</>
 	);
 }
