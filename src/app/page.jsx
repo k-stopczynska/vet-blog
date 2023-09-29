f5b909f133a7ac975922337e5d9a33c60e998b5d
k.stopczynska <k.stopczynska@gmail.com>
2023-09-29 12:04:56 +0200

    added rootlayout debug 13

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,15 +1,16 @@
 import Featured from '@/components/Featured';
 import CardList from '@/components/CardList';
 import CategoryList from '@/components/CategoryList';
+import RootLayout from './layout';
 
 export default function Home({ searchParams }) {
 	const page = parseInt(searchParams.page) || 1;
 
 	return (
-		<>
+		<RootLayout>
 			<Featured />
 			<CategoryList />
 			<CardList page={page} />
-		</>
+		</RootLayout>
 	);
 }
