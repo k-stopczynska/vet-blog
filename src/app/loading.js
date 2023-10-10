c04e97453f3d91025b31443ce3d0e115d5d347ee
k.stopczynska <k.stopczynska@gmail.com>
2023-10-10 14:21:57 +0200

    testing loading state for pages

--- src/app/loading.js
+++ src/app/loading.js
@@ -4,8 +4,9 @@ import { JellyTriangle } from '@uiball/loaders';
 
 
 const Loading = () => {
-  return (
-    <JellyTriangle size={100} speed={1.75} color='white' />
+    return (
+      // <JellyTriangle size={100} speed={1.75} color='white' />
+      <div>Loading...</div>
   )
 }
 
