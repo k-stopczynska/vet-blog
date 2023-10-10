669890636a985bdbf0fe82c94a4cf9145d8ca7a0
k.stopczynska <k.stopczynska@gmail.com>
2023-10-10 15:54:24 +0200

    changed link to router to reload page

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,14 +1,16 @@
-import React from 'react';
+import React, {useRouter} from 'react';
 import Image from 'next/image';
-import Link from 'next/link';
+import Button from '@/UI/Button';
 
 
 const NotFound = () => {
-  return (
+    const router = useRouter();
+    return (
       <div className="min-h-[400px] w-full relative">
         <Image src='/door.jpg' fill alt="not-found page" className='absolute bottom-0 left-0'/>
-        <div className="absolute bottom-10 left-0 flex items-center justify-center w-full h-20">
-            <h1>There is nothing for you to see, just go back...</h1><Link href='/'>Go back to main</Link>
+        <div className="absolute bottom-4 left-0 flex items-center justify-center w-full h-20">
+              <h1>There is nothing for you to see, just go back...</h1>
+            <Button title="Go back" variant="simpleButton" onClick={() => router.push('/')} type="button"/>
         </div>
       </div>
   )
