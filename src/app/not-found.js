6dcd049dfb9fe95f1a039eb1ec8a556f7984cc4c
k.stopczynska <k.stopczynska@gmail.com>
2023-10-10 15:29:59 +0200

    added fixed height

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -5,7 +5,7 @@ import LinkItem from '@/UI/LinkItem';
 
 const NotFound = () => {
   return (
-      <div className="h-full w-full relative">
+      <div className="h-[400px] w-full relative">
         <Image src='/door.jpg' fill alt="not-found page" className='absolute bottom-0 left-0'/>
         <div className="absolute bottom-10 left-0 flex items-center justify-center w-full h-20">
             <h1>There is nothing for you to see, just go back...</h1><LinkItem url='/' variant="CTA" title="Go back" />
