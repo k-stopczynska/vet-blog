7c62813487a6d7d3cd3ff4ddb2baee0295f5cb89
k.stopczynska <k.stopczynska@gmail.com>
2023-10-10 15:57:38 +0200

    chenged text

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -10,8 +10,8 @@ const NotFound = () => {
       <div className="min-h-[400px] w-full relative">
         <Image src='/door.jpg' fill alt="not-found page" className='absolute bottom-0 left-0'/>
         <div className="absolute bottom-4 left-0 flex items-center justify-center w-full h-20">
-              <h1>There is nothing for you to see, just go back...</h1>
-            <Button title="Go back" variant="simpleButton" onClick={() => router.push('/')} type="button"/>
+              <h1>There is nothing for you to see, just</h1>
+            <Button title="Go back..." variant="simpleButton" onClick={() => router.push('/')} type="button"/>
         </div>
       </div>
   )
