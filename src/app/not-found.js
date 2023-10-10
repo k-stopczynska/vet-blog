daf41b803fce3e5e5654df1b75212197075e8f05
k.stopczynska <k.stopczynska@gmail.com>
2023-10-10 15:12:53 +0200

    added not found page

---
+++ src/app/not-found.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Image from 'next/image';
+import LinkItem from '@/UI/LinkItem';
+
+
+const NotFound = () => {
+  return (
+      <div className="h-full w-full relative">
+        <Image src='/door.jpg' fill />
+        <div className="absolute bottom-10 left-0 flex items-center justify-center w-full h-20">
+            <h1>There is nothing for you to see, just go back...</h1><LinkItem href='/' variant="CTA" title="Go back" />
+        </div>
+      </div>
+  )
+}
+
+export default NotFound
