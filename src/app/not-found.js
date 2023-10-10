fa5b185d7c445d0b4d92fa2a7b341e38384c59a1
k.stopczynska <k.stopczynska@gmail.com>
2023-10-10 15:40:47 +0200

    testing links

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import Image from 'next/image';
-import LinkItem from '@/UI/LinkItem';
+import Link from 'next/link';
 
 
 const NotFound = () => {
   return (
-      <div className="h-[400px] w-full relative">
+      <div className="min-h-[400px] w-full relative">
         <Image src='/door.jpg' fill alt="not-found page" className='absolute bottom-0 left-0'/>
         <div className="absolute bottom-10 left-0 flex items-center justify-center w-full h-20">
-            <h1>There is nothing for you to see, just go back...</h1><LinkItem url='/' variant="CTA" title="Go back" />
+            <h1>There is nothing for you to see, just go back...</h1><Link href='/'>Go back to main</Link>
         </div>
       </div>
   )
