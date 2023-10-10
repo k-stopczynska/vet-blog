36be3b4f162b401ac9beb98e6512ebfc76db4ddc
k.stopczynska <k.stopczynska@gmail.com>
2023-10-10 15:25:27 +0200

    styles for not found img

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -6,7 +6,7 @@ import LinkItem from '@/UI/LinkItem';
 const NotFound = () => {
   return (
       <div className="h-full w-full relative">
-        <Image src='/door.jpg' fill alt="not-found page"/>
+        <Image src='/door.jpg' fill alt="not-found page" className='absolute bottom-0 left-0'/>
         <div className="absolute bottom-10 left-0 flex items-center justify-center w-full h-20">
             <h1>There is nothing for you to see, just go back...</h1><LinkItem url='/' variant="CTA" title="Go back" />
         </div>
