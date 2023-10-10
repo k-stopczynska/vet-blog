9b96709844988415db71998f007c81c65fdf8d84
k.stopczynska <k.stopczynska@gmail.com>
2023-10-10 14:44:57 +0200

    changing footer styles to make it stick to the bottom

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,9 +2,14 @@ import React from 'react';
 import Link from 'next/link';
 
 const Footer = () => {
-  return (
-    <footer className="w-full h-10 flex items-center justify-center gap-4">Copyright @2023 <Link href="https://github.com/k-stopczynska" className="link" >k-stopczynska</Link> </footer>
-  )
-}
+	return (
+		<footer className='w-full h-10 absolute bottom-0 left-0 flex items-center justify-center gap-4'>
+			Copyright @2023
+			<Link href='https://github.com/k-stopczynska' className='link'>
+				k-stopczynska
+			</Link>
+		</footer>
+	);
+};
 
-export default Footer
+export default Footer;
