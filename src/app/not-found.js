7a78e72c0942678e0034929e327861a3b46b231b
k.stopczynska <k.stopczynska@gmail.com>
2023-10-10 16:15:54 +0200

    changed button to standard anchor

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -13,7 +13,8 @@ const NotFound = () => {
         <Image src='/door.jpg' fill alt="not-found page" className='absolute bottom-0 left-0'/>
         <div className="absolute bottom-4 left-0 flex items-center justify-center w-full h-20">
               <h1>There is nothing for you to see, just</h1>
-            <Button title="Go back..." variant="simpleButton" onClick={() => router.push('/')} type="button"/>
+                <Button title="Go back..." variant="simpleButton" onClick={() => router.push('/')} type="button" />
+                <a href="/">go back...</a>
         </div>
       </div>
   )
