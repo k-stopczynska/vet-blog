30bb0f48d963683887557f61773307705240ffc2
k.stopczynska <k.stopczynska@gmail.com>
2023-10-10 14:50:54 +0200

    reversed changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 
 const Footer = () => {
 	return (
-		<footer className='w-full h-10 absolute bottom-0 left-0 flex items-center justify-center gap-4'>
+		<footer className='w-full h-10 flex items-center justify-center gap-4'>
 			Copyright @2023
 			<Link href='https://github.com/k-stopczynska' className='link'>
 				k-stopczynska
