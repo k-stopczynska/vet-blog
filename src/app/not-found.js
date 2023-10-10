5814547d5e460ce5292d087fb514e23c10a1fdab
k.stopczynska <k.stopczynska@gmail.com>
2023-10-10 16:21:29 +0200

    clean up unnneccesary imports

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,14 +1,12 @@
 'use client';
 
 import React from 'react';
-import { useRouter } from 'next/navigation';
 import Image from 'next/image';
 
 
 const NotFound = () => {
-    const router = useRouter();
     return (
-      <div className="min-h-[400px] w-full relative">
+      <div className="min-h-[400px] w-full relative object-cover">
         <Image src='/door.jpg' fill alt="not-found page" className='absolute bottom-0 left-0'/>
         <div className="absolute bottom-4 left-0 flex items-center justify-center w-full h-20 gap-4">
               <h1>There is nothing for you to see, just</h1>
