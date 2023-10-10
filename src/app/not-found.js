f5f681e35b9a85d789e1fd2722156ca5b6fabfed
k.stopczynska <k.stopczynska@gmail.com>
2023-10-10 15:17:46 +0200

    added alt for image

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -6,7 +6,7 @@ import LinkItem from '@/UI/LinkItem';
 const NotFound = () => {
   return (
       <div className="h-full w-full relative">
-        <Image src='/door.jpg' fill />
+        <Image src='/door.jpg' fill alt="not-found page"/>
         <div className="absolute bottom-10 left-0 flex items-center justify-center w-full h-20">
             <h1>There is nothing for you to see, just go back...</h1><LinkItem url='/' variant="CTA" title="Go back" />
         </div>
