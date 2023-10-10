8a61f5600ecd1e83347e6f0c4a02e843433d7e34
k.stopczynska <k.stopczynska@gmail.com>
2023-10-10 14:36:37 +0200

    added styles for loading

--- src/app/loading.js
+++ src/app/loading.js
@@ -5,10 +5,9 @@ import { JellyTriangle } from '@uiball/loaders';
 
 const Loading = () => {
   return (
-      <>
+      <div className="h-full w-full flex items-center justify-center">
         <JellyTriangle size={100} speed={1.75} color='white' />
-        <div>Loading...</div>
-      </>
+      </div>
   )
 }
 
